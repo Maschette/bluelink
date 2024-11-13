701237c251e8ac7876035979891a28b0193b0684
Maschette <dale.maschette@aad.gov.au>
2024-11-14 10:49:38 +1100

    Add data frame of depth layers

---
+++ R/bluelink_data.R
@@ -0,0 +1,17 @@
+#' Contextual data for Southern Ocean maps
+#'
+#' Various spatial datasets that are commonly used on bluelink data
+#'
+#' @docType data
+#'
+#' @usage data(bluelink_data)
+#'
+#' @format A list containing the following elements:
+#' \itemize{
+#'   \item depth_layers
+#'     \itemize{
+#'       \item Description: data frame of model depth layers layer and mid depth are from WMS data layers. Min and Max are calculated as the mid point between mid depths.
+#'     }
+#' }
+#' @keywords datasets
+"bluelink_data"
